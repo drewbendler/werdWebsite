4ad298a5e7ce4a191bec3402a45a111a5f4d08e2
Drew Bendler <drewbendler5@gmail.com>
2020-02-18 00:33:41 -0500

    fancier css

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ for (let i = 0; i < 10; i++) {
 
 	console.log('i love nijeh');
 }
+
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.querySelector("header").style.fontSize = "30px";
+  } else {
+    document.getElementById("header").style.fontSize = "90px";
+  }
+}
